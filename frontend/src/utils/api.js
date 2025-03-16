9b4b5c158b7b6a41434b0a65d21f5e10d62dbb6b
Nitin Pratap <122565643+Nitinpratap22061@users.noreply.github.com>
2025-03-16 16:50:12 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000"; // Replace with your backend URL
+const API_URL = "https://lastrepo-6nm3.onrender.com"; // Replace with your backend URL
 
 export const loginWithGitHub = () => {
   window.location.href = `${API_URL}/auth/github`;
