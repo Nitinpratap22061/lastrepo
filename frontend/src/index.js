ce4b577073ebd7f795c34ba2746b6e190872e7b7
Nitin Pratap <122565643+Nitinpratap22061@users.noreply.github.com>
2025-03-17 12:07:39 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client"; // Correct import for React 18+
 import App from "./App";
 import "./styles/index.css";
-//import './styles.css';
+import './style.css';
 
 // Create a root and render the app
 const root = ReactDOM.createRoot(document.getElementById("root"));
